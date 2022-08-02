2abab2fcd4e1fdce15d960b625ce9a8561a56cd9
ruby <jhonnatanwfc@live.com>
2022-08-02 16:57:35 -0300

    ball (set up)

--- main.lua
+++ main.lua
@@ -9,7 +9,12 @@ function love.load()
 	right = {} -- the right paddle 
 	right.x = win_w - rect_w
 	right.y = win_h / 2 - rect_h / 2
-	vel = 500 -- paddles' speed 
+	rect_vel = 500 -- paddles' speed 
+	ball = {} -- game's ball
+	ball.x = win_w / 2
+	ball.y = win_h / 2
+	ball.vx = 0 -- horizontal velocity
+	ball.vy = 0 -- vertical velocity
 end
 
 function love.draw()
@@ -26,14 +31,14 @@ end
 
 function love.update(dt)
 	if love.keyboard.isDown('w') then 
-		left.y = left. y - vel * dt 
+		left.y = left. y - rect_vel * dt 
 	elseif love.keyboard.isDown('s') then 
-		left.y = left.y + vel * dt 
+		left.y = left.y + rect_vel * dt 
 	end 
 	if love.keyboard.isDown('up') then 
-		right.y = right.y - vel * dt 
+		right.y = right.y - rect_vel * dt 
 	elseif love.keyboard.isDown('down') then 
-		right.y = right.y + vel * dt
+		right.y = right.y + rect_vel * dt
 	end 
 	if left.y < 0 then 
 		left.y = 0
